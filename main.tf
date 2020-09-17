3a9ea073db86ec2d16ccb226304fd957fb1c511d
k8ship <support@k8ship.com>
2020-09-17 11:10:56 -0400

    add main config file

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_provider{
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-098f16afa9edf40be"
+  instance_type = "t2.micro"
